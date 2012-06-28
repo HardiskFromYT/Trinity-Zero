49642fa32492534363101d384d69098f2f4151ac
Alternative <Alternative@masschaos-gm.com>
2012-06-28 02:20:37 -0400

    Authserver should now only accept pre-TBC builds

--- src/server/authserver/Authentication/AuthCodes.cpp
+++ src/server/authserver/Authentication/AuthCodes.cpp
@@ -19,7 +19,7 @@
 
 namespace AuthHelper
 {
-    bool IsPreBCAcceptedClientBuild(int build)
+    /*bool IsPreBCAcceptedClientBuild(int build)
     {
         int accepted_versions[] = PRE_BC_ACCEPTED_CLIENT_BUILD;
 
@@ -39,10 +39,21 @@ namespace AuthHelper
                 return true;
 
         return false;
-    }
+    }*/
 
     bool IsAcceptedClientBuild(int build)
     {
         return (IsPostBCAcceptedClientBuild(build) || IsPreBCAcceptedClientBuild(build));
+    }
+
+    bool IsAcceptedClientBuild(int build)
+    {
+        int accepted_versions[] = ACCEPTED_CLIENT_BUILD;
+
+        for (int i = 0; accepted_versions[i]; ++i)
+            if (build == accepted_versions[i])
+                return true;
+
+        return false;
     }
 };
